f144fdce2dde5549d1ecee148d7b4c0229df442d
Tyler Roberts <steventylerfive@gmail.com>
2016-05-18 11:23:29 -0500

    Recreated database and added classes to mirror the tables

---
+++ src/Bangazon_MVC/Controllers/BangazonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Bangazon_MVC.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bangazon_MVC.Controllers
+{
+    public class BangazonController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            var query = from c in _context.Customer
+
+                        select c;
+
+            var employeeDepartment = (from emp in _myEmployeeContext.Employee
+                                      join dept in _myEmployeeContext.Department
+                                      on emp.DepartmentId equals dept.DepartmentId
+                                      orderby dept.DepartmentName
+                                      select new EmployeeDetailViewModel
+                                      {
+                                          Name = emp.Name,
+                                          EmployeeId = emp.EmployeeId,
+                                          DepartmentName = dept.DepartmentName
+                                      }).ToList();
+
+            EmployeeDepartmentDetailViewModel employeeDetails = new EmployeeDepartmentDetailViewModel
+            {
+                Employees = employeeDepartment
+            };
+
+            return View(employeeDetails);
+        }
+    }
+}
---
+++ src/Bangazon_MVC/Models/OrderProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_MVC.Models
+{
+    public class OrderProducts
+    {
+        public int IdOrderProducts { get; set; }
+        public int IdProduct { get; set; }
+        public int IdCustomerOrder { get; set; }
+    }
+}
